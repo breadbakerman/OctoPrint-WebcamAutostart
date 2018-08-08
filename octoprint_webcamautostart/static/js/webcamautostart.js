d1a41161b5b18b21b03e1f13203cf99b1857eb36
breadbakerman <breadbaker@gmail.com>
2018-08-08 13:06:59 +0200

    Do something.

--- octoprint_webcamautostart/static/js/webcamautostart.js
+++ octoprint_webcamautostart/static/js/webcamautostart.js
@@ -8,12 +8,30 @@ $(function() {
     function WebcamautostartViewModel(parameters) {
         var self = this;
         self.settings = parameters[0];
-
-        self.onBeforeBinding = function () {
-            console.log('-----------------> WEBCAM AUTOSTART');
+        
+        self.enableWebcam = function () {
+            var autostartWebcam = parseInt(self.settings.settings.plugins.webcamautostart.autostartWebcam());
+            if (!autostartWebcam) {
+                return
+            }
+            console.log('Automatically starting webcam...');
+            OctoPrint.system.executeCommand('custom', 'webcamstart');
         }
+        
+        self.disableWebcam = function () {
+            var autostopWebcam = parseInt(self.settings.settings.plugins.webcamautostart.autostopWebcam());
+            if (!autostopWebcam) {
+                return
+            }
+            console.log('Automatically stopping webcam...');
+            OctoPrint.system.executeCommand('custom', 'webcamstop');
+        }
+        
+        self.onEventPrintStarted = self.enableWebcam;
+        self.onEventPrintDone = self.disableWebcam;
+        self.onEventPrintFailed = self.disableWebcam;
     }
-
+    
     OCTOPRINT_VIEWMODELS.push({
         construct: WebcamautostartViewModel,
         dependencies: [ 'settingsViewModel' ],
